6f8740c65091f46de5bea936363a6799d8421649
Zar Nie Aung <zarzukyi3@gmail.com>
2024-12-19 00:18:11 +0800

    Got chess pieces
    Attempted to make a bigger grid but failed.

--- src/main/scala-3/ChessTest.scala
+++ src/main/scala-3/ChessTest.scala
@@ -1,3 +1,6 @@
+import ChessPiece.*
+import Color.*
+
 /** BY: ZAR NIE AUNG (BSE_21074380)
  *
  *
@@ -17,32 +20,116 @@
  */
 
 enum Color:
-  case white, black, none
+  case White, Black, Blank
 
-class ChessPiece
+enum ChessPiece:
+  case Pawn, Bishop, Knight, Rook, Queen, King
+
+case class BoardPiece(var chessPiece: ChessPiece, var color: Color):
+  def symbol: String =
+    chessPiece match
+      case Pawn => if color == White then "♙" else "♟"
+      case Bishop => if color == White then "♗" else "♝"
+      case Knight => if color == White then "♘" else "♞"
+      case Rook => if color == White then "♖" else "♜"
+      case Queen => if color == White then "♕" else "♛"
+      case King => if color == White then "♔" else "♚"
+
+
+
+//Not a clue how to do it yet, will hold it off for now
+//enum Piece:
+//  case Pawn, Knight, Bishop, Rook, Queen, King
+//
+//trait ChessSymbol:
+//  def color: Color
+//  def symbol: String
+//  def piece: Piece
+//  def legalMoves (position: (Int, Int), board: Board): List[(Int, Int)]
+//
+////enum ChessSymbol:
+////  case ♜, ♞, ♝, ♛, ♚, ♟, ♖, ♘, ♗, ♕, ♔, ♙
+//
+//case class ChessPiece(val color: Color)
+
+//trait ChessSymbol:
+//  def symbol: String
+
+
+//trait FullDiagonal
+//trait FullBar
+//trait KnightJump
+//trait Promote
+//trait EnPassant
+//trait CastleAble
+//
+//object Rook extends FullBar
+//object Bishop extends FullDiagonal
+//object Queen extends FullBar, FullDiagonal
 
 object ChessPiece:
-  val color = Color
+  private val color = Color
 
-type Board = Array[Array[Option[ChessPiece]]]
+type Board = Array[Array[Option[BoardPiece]]]
 
 object MyApp extends App:
 
-  def buildBoard(): Board =
-    val board = Array.fill(8, 8)(Option.empty[ChessPiece])
-    return board
+  private def buildBoard(): Board =
+    val board = Array.fill(8, 8)(Option.empty[BoardPiece])
+
+    // Major Pieces
+    val majorPiecesBlack = Array(
+      BoardPiece(ChessPiece.Rook, Color.Black),
+      BoardPiece(ChessPiece.Knight, Color.Black),
+      BoardPiece(ChessPiece.Bishop, Color.Black),
+      BoardPiece(ChessPiece.King, Color.Black),
+      BoardPiece(ChessPiece.Queen, Color.Black),
+      BoardPiece(ChessPiece.Bishop, Color.Black),
+      BoardPiece(ChessPiece.Knight, Color.Black),
+      BoardPiece(ChessPiece.Rook, Color.Black)
+    )
+
+    val majorPiecesWhite = Array(
+      BoardPiece(ChessPiece.Rook, Color.White),
+      BoardPiece(ChessPiece.Knight, Color.White),
+      BoardPiece(ChessPiece.Bishop, Color.White),
+      BoardPiece(ChessPiece.King, Color.White),
+      BoardPiece(ChessPiece.Queen, Color.White),
+      BoardPiece(ChessPiece.Bishop, Color.White),
+      BoardPiece(ChessPiece.Knight, Color.White),
+      BoardPiece(ChessPiece.Rook, Color.White)
+    )
+
+    for i <- 0 until 8 do
+      board(0)(i) = Some(majorPiecesBlack(i))
+      board(1)(i) = Some(BoardPiece(Pawn, Black)) //black pawns
+      board(6)(i) = Some(BoardPiece(Pawn, White)) //white pawns
+      board(7)(i) = Some(majorPiecesWhite(i))
+
+    board
   end buildBoard
 
-  def printBoard(board: Board): Unit =
+  private def printBoard(board: Board): Unit =
     for row <- board do
       println(row.map{
-        case Some(piece) => "W"
-        case None => "[ ]"
-      }.mkString(" "))
-      println()
+        case Some(piece) => f"[${piece.symbol}]"
+        case None => "[#]"
+      }.mkString)
+//      // Square Top
+//      println(row.map(_ => " ___").mkString(" "))
+//
+//      // Square Middle
+//      println(row.map {
+//        case Some(piece) => f"| ${piece.symbol} |" //Filled Square
+//        case None => "|    |" // Empty square
+//      }.mkString(""))
+//
+//      // Square Bottom
+//      println(row.map(_ => "|___|").mkString(""))
+    end for
   end printBoard
 
-  val board = buildBoard()
+  private val board = buildBoard()
   printBoard(board)
 
 
