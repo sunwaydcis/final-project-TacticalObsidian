916d338b37cb9637f496e3c2ccdc799e44641ccb
Zar Nie Aung <zarzukyi3@gmail.com>
2024-12-19 19:44:10 +0800

    Attempt 2
    This attempt ultimately got me no where.
    
    Made a revision to the UML class diagram. Now includes Dev defined Factories to facilitate class creations.

--- src/main/scala-3/ChessTest.scala
+++ src/main/scala-3/ChessTest.scala
@@ -1,4 +1,4 @@
-import ChessPiece.*
+import Symbol.*
 import Color.*
 
 /** BY: ZAR NIE AUNG (BSE_21074380)
@@ -19,15 +19,25 @@ import Color.*
  *  The purpose of this code is to test the feasibility of the idea, using the Scala CLI terminal as the main UI component.
  */
 
+//To make typecasting much easier and less verbose
+type Board = Array[Array[Option[BoardPiece]]]
+type Position = (Int, Int)
+
 enum Color:
   case White, Black, Blank
 
-enum ChessPiece:
+
+enum Symbol:
   case Pawn, Bishop, Knight, Rook, Queen, King
 
-case class BoardPiece(var chessPiece: ChessPiece, var color: Color):
+trait ChessPiece:
+  def symbol: String
+  def color: Color
+  def move(): Unit
+
+case class BoardPiece(var chessSymbol: Symbol, var color: Color):
   def symbol: String =
-    chessPiece match
+    chessSymbol match
       case Pawn => if color == White then "♙" else "♟"
       case Bishop => if color == White then "♗" else "♝"
       case Knight => if color == White then "♘" else "♞"
@@ -35,42 +45,18 @@ case class BoardPiece(var chessPiece: ChessPiece, var color: Color):
       case Queen => if color == White then "♕" else "♛"
       case King => if color == White then "♔" else "♚"
 
-
-
-//Not a clue how to do it yet, will hold it off for now
-//enum Piece:
-//  case Pawn, Knight, Bishop, Rook, Queen, King
-//
-//trait ChessSymbol:
-//  def color: Color
-//  def symbol: String
-//  def piece: Piece
-//  def legalMoves (position: (Int, Int), board: Board): List[(Int, Int)]
-//
-////enum ChessSymbol:
-////  case ♜, ♞, ♝, ♛, ♚, ♟, ♖, ♘, ♗, ♕, ♔, ♙
-//
-//case class ChessPiece(val color: Color)
-
-//trait ChessSymbol:
-//  def symbol: String
-
-
-//trait FullDiagonal
-//trait FullBar
-//trait KnightJump
-//trait Promote
-//trait EnPassant
-//trait CastleAble
-//
-//object Rook extends FullBar
-//object Bishop extends FullDiagonal
-//object Queen extends FullBar, FullDiagonal
-
-object ChessPiece:
+object Symbol:
   private val color = Color
 
-type Board = Array[Array[Option[BoardPiece]]]
+//UPGRADE
+trait ChessPiece2:
+  def symbol: String
+  def color: Color
+  def position: (Int, Int)
+  def legalMoves(position: Position = this.position, board: Board): List[Position]
+
+
+
 
 object MyApp extends App:
 
@@ -79,25 +65,25 @@ object MyApp extends App:
 
     // Major Pieces
     val majorPiecesBlack = Array(
-      BoardPiece(ChessPiece.Rook, Color.Black),
-      BoardPiece(ChessPiece.Knight, Color.Black),
-      BoardPiece(ChessPiece.Bishop, Color.Black),
-      BoardPiece(ChessPiece.King, Color.Black),
-      BoardPiece(ChessPiece.Queen, Color.Black),
-      BoardPiece(ChessPiece.Bishop, Color.Black),
-      BoardPiece(ChessPiece.Knight, Color.Black),
-      BoardPiece(ChessPiece.Rook, Color.Black)
+      BoardPiece(Rook, Black),
+      BoardPiece(Knight, Black),
+      BoardPiece(Bishop, Black),
+      BoardPiece(King, Black),
+      BoardPiece(Queen, Black),
+      BoardPiece(Bishop, Black),
+      BoardPiece(Knight, Black),
+      BoardPiece(Rook, Black)
     )
 
     val majorPiecesWhite = Array(
-      BoardPiece(ChessPiece.Rook, Color.White),
-      BoardPiece(ChessPiece.Knight, Color.White),
-      BoardPiece(ChessPiece.Bishop, Color.White),
-      BoardPiece(ChessPiece.King, Color.White),
-      BoardPiece(ChessPiece.Queen, Color.White),
-      BoardPiece(ChessPiece.Bishop, Color.White),
-      BoardPiece(ChessPiece.Knight, Color.White),
-      BoardPiece(ChessPiece.Rook, Color.White)
+      BoardPiece(Rook, White),
+      BoardPiece(Knight, White),
+      BoardPiece(Bishop, White),
+      BoardPiece(King, White),
+      BoardPiece(Queen, White),
+      BoardPiece(Bishop, White),
+      BoardPiece(Knight, White),
+      BoardPiece(Rook, White)
     )
 
     for i <- 0 until 8 do
@@ -115,6 +101,63 @@ object MyApp extends App:
         case Some(piece) => f"[${piece.symbol}]"
         case None => "[#]"
       }.mkString)
+    end for
+  end printBoard
+
+  private val board = buildBoard()
+  printBoard(board)
+
+//Not a clue how to do it yet, will hold it off for now
+////Use factories to centralize development logic
+//object BoardFactory:
+//  def standardBoard(): Board =
+//    val board = Array.fill(8, 8)(Option.empty[BoardPiece])
+//
+//    for i <- 0 until 8 do
+//
+//      end for
+//    board
+//
+//
+//object ChessPieceFactory:
+//  def createNewPiece(piece: Symbol, color: Color): BoardPiece =
+//    piece match
+//      case Pawn =>  BoardPiece(color)
+//      case Bishop =>
+//      case Knight =>
+//      case Rook =>
+//      case Queen =>
+//      case King =>
+//      case _ =>
+//enum Piece:
+//  case Pawn, Knight, Bishop, Rook, Queen, King
+//
+//trait ChessSymbol:
+//  def color: Color
+//  def symbol: String
+//  def piece: Piece
+//  def legalMoves (position: (Int, Int), board: Board): List[(Int, Int)]
+//
+////enum ChessSymbol:
+////  case ♜, ♞, ♝, ♛, ♚, ♟, ♖, ♘, ♗, ♕, ♔, ♙
+//
+//case class Symbol(val color: Color)
+
+//trait ChessSymbol:
+//  def symbol: String
+
+
+//trait FullDiagonal
+//trait FullBar
+//trait KnightJump
+//trait Promote
+//trait EnPassant
+//trait CastleAble
+//
+//object Rook extends FullBar
+//object Bishop extends FullDiagonal
+//object Queen extends FullBar, FullDiagonal
+
 //      // Square Top
 //      println(row.map(_ => " ___").mkString(" "))
 //
@@ -126,14 +169,3 @@ object MyApp extends App:
 //
 //      // Square Bottom
 //      println(row.map(_ => "|___|").mkString(""))
-    end for
-  end printBoard
-
-  private val board = buildBoard()
-  printBoard(board)
-
-
-
-
-
-  buildBoard()
