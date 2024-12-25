a48277de286b2fc051474748e5f08bc6d6fc81ca
Zar Nie Aung <zarzukyi3@gmail.com>
2024-12-25 19:40:10 +0800

    Just Suck It Up - Push

--- src/main/scala-3/ChessTest.scala
+++ src/main/scala-3/ChessTest.scala
@@ -1,171 +1,37 @@
-import Symbol.*
-import Color.*
-
-/** BY: ZAR NIE AUNG (BSE_21074380)
- *
- *
- * Good day Dr Chin, for my OOP FA, I aim to develop a simple AI chess program.
- * The main focus of this code is to offer users a personalized AI opponent, that will always be:
- * (50 - 1000 ELO) above the user's skill grade to offer a more personalized challenge and facilitate learning.
- *
- * The reasoning behind my project is because many mainstream chess games, most popular being chess.com,
- * is that they do not offer such learning advantages. Players must find where they belong first, and deal with an
- * AI bot that is often either too high or too low. I want to offer users (myself primarily) with a software
- * that I can use to learn chess with a challenge that facilitates my learning needs best.
- *
- * Yes, there still carries a lot of benefit for having these other chess bots to fight against,
- * at least 3 AI bots will remain.
- *
- *  The purpose of this code is to test the feasibility of the idea, using the Scala CLI terminal as the main UI component.
- */
-
-//To make typecasting much easier and less verbose
-type Board = Array[Array[Option[BoardPiece]]]
-type Position = (Int, Int)
-
-enum Color:
-  case White, Black, Blank
-
-
-enum Symbol:
-  case Pawn, Bishop, Knight, Rook, Queen, King
-
-trait ChessPiece:
-  def symbol: String
-  def color: Color
-  def move(): Unit
-
-case class BoardPiece(var chessSymbol: Symbol, var color: Color):
-  def symbol: String =
-    chessSymbol match
-      case Pawn => if color == White then "♙" else "♟"
-      case Bishop => if color == White then "♗" else "♝"
-      case Knight => if color == White then "♘" else "♞"
-      case Rook => if color == White then "♖" else "♜"
-      case Queen => if color == White then "♕" else "♛"
-      case King => if color == White then "♔" else "♚"
-
-object Symbol:
-  private val color = Color
-
-//UPGRADE
-trait ChessPiece2:
-  def symbol: String
-  def color: Color
-  def position: (Int, Int)
-  def legalMoves(position: Position = this.position, board: Board): List[Position]
+import scalafx.application.JFXApp3
+import scalafx.scene.Scene
+import scalafx.scene.layout.GridPane
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
 
 
 
+object ChessBoardApp extends JFXApp3 {
 
-object MyApp extends App:
+  override def start(): Unit = {
+    // Create the main board
+    val board = new GridPane
 
-  private def buildBoard(): Board =
-    val board = Array.fill(8, 8)(Option.empty[BoardPiece])
+    // Define cell size
+    val cellSize = 60
 
-    // Major Pieces
-    val majorPiecesBlack = Array(
-      BoardPiece(Rook, Black),
-      BoardPiece(Knight, Black),
-      BoardPiece(Bishop, Black),
-      BoardPiece(King, Black),
-      BoardPiece(Queen, Black),
-      BoardPiece(Bishop, Black),
-      BoardPiece(Knight, Black),
-      BoardPiece(Rook, Black)
-    )
+    // Create the 8x8 board with alternating colors
+    for (row <- 0 until 8; col <- 0 until 8) {
+      val cellColor = if ((row + col) % 2 == 0) Color.White else Color.Black
+      val cell = new Rectangle {
+        width = cellSize
+        height = cellSize
+        fill = cellColor
+      }
+      board.add(cell, col, row) // Add cell to the grid at (col, row)
+    }
 
-    val majorPiecesWhite = Array(
-      BoardPiece(Rook, White),
-      BoardPiece(Knight, White),
-      BoardPiece(Bishop, White),
-      BoardPiece(King, White),
-      BoardPiece(Queen, White),
-      BoardPiece(Bishop, White),
-      BoardPiece(Knight, White),
-      BoardPiece(Rook, White)
-    )
-
-    for i <- 0 until 8 do
-      board(0)(i) = Some(majorPiecesBlack(i))
-      board(1)(i) = Some(BoardPiece(Pawn, Black)) //black pawns
-      board(6)(i) = Some(BoardPiece(Pawn, White)) //white pawns
-      board(7)(i) = Some(majorPiecesWhite(i))
-
-    board
-  end buildBoard
-
-  private def printBoard(board: Board): Unit =
-    for row <- board do
-      println(row.map{
-        case Some(piece) => f"[${piece.symbol}]"
-        case None => "[#]"
-      }.mkString)
-    end for
-  end printBoard
-
-  private val board = buildBoard()
-  printBoard(board)
-
-//Not a clue how to do it yet, will hold it off for now
-////Use factories to centralize development logic
-//object BoardFactory:
-//  def standardBoard(): Board =
-//    val board = Array.fill(8, 8)(Option.empty[BoardPiece])
-//
-//    for i <- 0 until 8 do
-//
-//      end for
-//    board
-//
-//
-//object ChessPieceFactory:
-//  def createNewPiece(piece: Symbol, color: Color): BoardPiece =
-//    piece match
-//      case Pawn =>  BoardPiece(color)
-//      case Bishop =>
-//      case Knight =>
-//      case Rook =>
-//      case Queen =>
-//      case King =>
-//      case _ =>
-//enum Piece:
-//  case Pawn, Knight, Bishop, Rook, Queen, King
-//
-//trait ChessSymbol:
-//  def color: Color
-//  def symbol: String
-//  def piece: Piece
-//  def legalMoves (position: (Int, Int), board: Board): List[(Int, Int)]
-//
-////enum ChessSymbol:
-////  case ♜, ♞, ♝, ♛, ♚, ♟, ♖, ♘, ♗, ♕, ♔, ♙
-//
-//case class Symbol(val color: Color)
-
-//trait ChessSymbol:
-//  def symbol: String
-
-
-//trait FullDiagonal
-//trait FullBar
-//trait KnightJump
-//trait Promote
-//trait EnPassant
-//trait CastleAble
-//
-//object Rook extends FullBar
-//object Bishop extends FullDiagonal
-//object Queen extends FullBar, FullDiagonal
-
-//      // Square Top
-//      println(row.map(_ => " ___").mkString(" "))
-//
-//      // Square Middle
-//      println(row.map {
-//        case Some(piece) => f"| ${piece.symbol} |" //Filled Square
-//        case None => "|    |" // Empty square
-//      }.mkString(""))
-//
-//      // Square Bottom
-//      println(row.map(_ => "|___|").mkString(""))
+    // Set up the scene
+    stage = new JFXApp3.PrimaryStage {
+      title = "Chess Board"
+      scene = new Scene {
+        content = board
+      }
+    }
+  }
+}
---
+++ src/main/scala-3/MainApp.scala
@@ -0,0 +1,47 @@
+package zn.makery.ChessTutor
+
+import javafx.fxml.FXMLLoader
+import scalafx.application.JFXApp3
+import scalafx.application.JFXApp3.PrimaryStage
+import scalafx.scene.Scene
+import scalafx.Includes.*
+import javafx.scene as jfxs
+import scalafx.collections.ObservableBuffer
+import scalafx.stage.{Modality, Stage}
+
+object ChessTutorApp extends JFXApp3:
+
+  //Window root pane
+  var rootPane: Option[scalafx.scene.layout.BorderPane] = None
+
+  override def start(): Unit =
+    val rootResource = getClass.getResource("view/RootLayout.fxml")
+    val loader = new FXMLLoader(rootResource)
+    loader.load()
+
+    rootPane = Option(loader.getRoot[jfxs.layout.BorderPane])
+    stage = new PrimaryStage():
+      title = "ChessTutor"
+      scene = new Scene():
+        root = rootPane.get
+
+    //For testing purposes show the game
+    showGame()
+
+  end start
+
+  //Actions (Methods) for ChessTutorApp
+  def showGame(): Unit =
+    val resource = getClass.getResource("view/GameView.fxml")
+    val loader = new FXMLLoader(resource)
+    loader.load()
+
+    val rootPane = loader.getRoot[jfxs.layout.AnchorPane]
+    this.rootPane.get.center = rootPane
+
+  end showGame
+
+  def showAlert(): Unit = ???
+  end showAlert
+
+end ChessTutorApp
