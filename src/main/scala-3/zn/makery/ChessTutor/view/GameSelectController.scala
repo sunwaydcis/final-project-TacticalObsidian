e82431afe66e5c9db9808742f5ec3901e90c344c
Zar Nie Aung <zarzukyi3@gmail.com>
2025-01-01 15:44:58 +0800

    Update GameSelectController.scala
    e

--- src/main/scala-3/zn/makery/ChessTutor/view/GameSelectController.scala
+++ src/main/scala-3/zn/makery/ChessTutor/view/GameSelectController.scala
@@ -1,17 +1,49 @@
 package zn.makery.ChessTutor.view
 
 import javafx.event.ActionEvent
+import scalafx.beans.property.{DoubleProperty, IntegerProperty, StringProperty}
 import zn.makery.ChessTutor.ChessTutorApp
+import scalafx.scene.control.{ToggleButton, ToggleGroup}
+
+enum Role:
+  case WHITE, BLACK, RANDOM
+
+enum Burst:
+  case UNLIMITED, STANDARD, BLITZ
+
+enum Mode:
+  case VsPlayer, VsBotEqual, VsBotEasy
 
 class GameSelectController():
   /** CODE RESPONSIBILITY - Control the view logic
-   *  The outcome of certain views, can lead to the creation of a game instance object.
+   * The outcome of certain views, can lead to the creation of a game instance object.
    */
 
-  def doPlay_Test(action: ActionEvent): Unit =
+  private var gameRole : StringProperty = new StringProperty()
+  private var playerMaxTime: DoubleProperty = new DoubleProperty()
+  private var botMaxTime: DoubleProperty = new DoubleProperty()
+  private var challenger: StringProperty = new StringProperty()
+  private var botEloAppend: IntegerProperty = new IntegerProperty()
+
+
+  private def doPlay_Test(action: ActionEvent): Unit =
     val onClick = ChessTutorApp.showGame()
   end doPlay_Test
 
-  def doBackTrack(action: ActionEvent): Unit =
+  private def doBackTrack(action: ActionEvent): Unit =
     val onClick = ChessTutorApp.showEntryPoint()
   end doBackTrack
+
+
+
+//  //Role changers
+//  private def startWhite(action: ActionEvent): Unit = ???
+//  private def startBlack(action: ActionEvent): Unit = ???
+//  private def startRandom(action: ActionEvent): Unit = ???
+//
+//  //Time changers
+//  private def beUnlimited(action: ActionEvent): Unit = ???
+//  private def beStandard(action: ActionEvent): Unit = ???
+//  private def beSeige(action: ActionEvent): Unit = ???
+//  private def beSeiged(action: ActionEvent): Unit = ???
+//  private def beBlitz(action: ActionEvent): Unit = ???
